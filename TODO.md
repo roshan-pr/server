3f254b53c8a29668e1fb79ce09c2777fa6bf29e9
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-06-22 18:40:56 +0530

    Separated html and response into different functions

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO
+
+  - [ ] Parse the request from the client
+
+
+# DONE
+
+  - [x] Separate html and response independent.
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const { createServer } = require('net');
 
-response = "HTTP/1.1 200\r\n\r\n<html><body><h1>Hello</h1></body></html>\r\n";
+const html = body => `<html><body><h1>${body}</h1></body></html>`;
+
+const response = html => `HTTP/1.1 200\r\n\r\n${html}\r\n`;
 
 const onNewConnection = (socket) => {
-
   socket.setEncoding('utf8');
-  socket.on('data', (data) => {
-    socket.write(response);
+  socket.on('data', (chunk) => {
+    socket.write(response(html('Server')));
     socket.end();
   });
 };
 
-const server = createServer(onNewConnection);
-
 const PORT = 8000;
+const server = createServer(onNewConnection);
 server.listen(PORT, () => console.log(`Server listening to ${PORT}`));
