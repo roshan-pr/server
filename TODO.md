f69736c7ca45a8d70d124d02fa6eab91d65f4411
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-06-23 07:33:44 +0530

    Added request handler

--- TODO.md
+++ TODO.md
@@ -1,10 +1,14 @@
 # TODO
 
-  - [ ] Add request handler
+  - [ ] Make the server independent
+    - [ ] Pass the request handler from the main
+    - [ ] Server takes arguments port and request handler
+  
 
 
 # DONE
 
+  - [x] Add request handler
   - [x] Move parser to separate file
   - [x] Parse header to lower case
   - [x] Parse the request from the client
--- src/server.js
+++ src/server.js
@@ -4,11 +4,19 @@ const html = body => `<html><body><h1>${body}</h1></body></html>`;
 
 const response = html => `HTTP/1.1 200\r\n\r\n${html}\r\n`;
 
+const requestHandler = ({ uri }, socket) => {
+  if (uri === '/') {
+    socket.write(response(html('Hello')));
+    return;
+  }
+  socket.write(response(html('unknown')));
+};
+
 const onNewConnection = (socket) => {
   socket.setEncoding('utf8');
   socket.on('data', (chunk) => {
     const request = parseRequest(chunk);
-    socket.write(response(html('Server')));
+    requestHandler(request, socket);
     socket.end();
   });
 };
